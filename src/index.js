47b03730b391c8d8808141c1641b4a3971838784
muhon9 <sultan.al.muhon@gmail.com>
2022-04-24 04:48:16 +0600

    Added play pause reset functionality

--- src/index.js
+++ src/index.js
@@ -1,15 +1,57 @@
 //Test import of styles. You can import Sass here if you use
 import "@/styles/style.css";
 
-const timeCounter = document.querySelector(".time-counter");
+const counterDisplay = document.querySelector(".time-counter");
+const playPauseBtn = document.querySelector(".play-pause-button");
+const resetBtn = document.querySelector(".reset-button");
 
-let time = 25 * 60 - 1;
+let timer;
+let sessionType = "session";
+let sessionLength = 25;
+let breakLength = 5;
+let time = 1500;
+let timerOn = false;
 
-function timer() {
-  let hour = Math.floor(time / 60);
-  let minutes = (time % 60).toFixed(0);
-  timeCounter.textContent = `${hour}:${minutes > 9 ? minutes : "0" + minutes}`;
-  time -= 1;
+function displayCount() {
+  let minutes = Math.floor(time / 60);
+  let seconds = (time % 60).toFixed(0);
+  counterDisplay.textContent = `${minutes}:${
+    seconds > 9 ? seconds : "0" + seconds
+  }`;
 }
 
-setInterval(timer, 1000);
+function setTime(newTime) {
+  time = newTime * 60;
+  displayCount();
+}
+
+function startTimer() {
+  clearInterval(timer);
+  timer = setInterval(() => {
+    time--;
+    displayCount();
+  }, 1000);
+}
+
+function handlePlayPause() {
+  if (timerOn) {
+    clearInterval(timer);
+    timerOn = false;
+    playPauseBtn.innerHTML = `<i class="fa-solid fa-play"></i>`;
+  } else {
+    startTimer();
+    timerOn = true;
+    playPauseBtn.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+  }
+}
+
+function handleReset() {
+  clearInterval(timer);
+  setTime(sessionLength);
+  playPauseBtn.innerHTML = `<i class="fa-solid fa-play"></i>`;
+  sessionType = "session";
+  timerOn = false;
+}
+
+playPauseBtn.addEventListener("click", handlePlayPause);
+resetBtn.addEventListener("click", handleReset);
